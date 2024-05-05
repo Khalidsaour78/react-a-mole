01626bb1f224ca22dc9090d33704cdfa39c15aa3
Khalid <Alsaoork@gmail.com>
2024-05-05 19:07:09 +0300

    Making a basic app

---
+++ src/Components/Empty.js
@@ -0,0 +1,13 @@
+
+import { useState, useEffect } from "react";
+import trace from "../helper";
+
+function Empty() {
+  return (
+    <div>
+      <span>Empty</span>
+    </div>
+  );
+}
+
+export default Empty;
---
+++ src/Components/Mole.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import trace from "../helper";
+
+function Mole() {
+  return (
+    <div>
+      <span>Mole</span>
+    </div>
+  );
+}
+
+export default Mole;
